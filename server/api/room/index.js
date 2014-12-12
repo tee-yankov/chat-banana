6be4bc58d5ad369740c8914d2b01a3082c56d4ec
Theodore Yankov <venoma333@gmail.com>
2014-12-12 14:37:36 +0200

    Secured room routes.

--- server/api/room/index.js
+++ server/api/room/index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var controller = require('./room.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
@@ -9,8 +10,8 @@ router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.post('/:id', controller.createmessage);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.put('/:id', auth.hasRole('admin'), controller.update);
+router.patch('/:id', auth.hasRole('admin'), controller.update);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
